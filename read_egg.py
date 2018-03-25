01d1373463af36998f30429dd0cb25c490183545
Larry Liu <yunpengliu199703@gmail.com>
2018-03-25 02:40:25 +0000

    initial update

--- read_egg.py
+++ read_egg.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy as np
 
-imgBGR = cv2.imread('image_face.jpg')
+imgBGR = cv2.imread('photos/girl_brownBackground.jpg')
 destRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
-sampleColor = destRGB[1][1]
-print(destRGB[1])
-for i in range(500):
-    for j in range(500):
-        if(destRGB[i][j] == sampleColor):
-            print("face detected")
+#print(destRGB)
+print(destRGB.shape)
+print(destRGB.max())
+print(destRGB[2,3])
+#sampleColor = destRGB[1][1]
+#print(destRGB[1])
+#for i in range(500):
+#    for j in range(500):
+#        if(destRGB[i][j] == sampleColor):
+#            print("face detected")
