98c19c05916f9e6e90c7b93e1ce139a8ae8500b3
Larry Liu <yunpengliu199703@gmail.com>
2018-03-25 01:45:08 +0000

    allowing parameter for picamera

---
+++ camera_pi.py
@@ -0,0 +1,12 @@
+import time
+import picamera
+import sys
+print(sys.argv[1])
+camera = picamera.PiCamera()
+camera.start_preview()
+file_header = "photos/"
+file_footer = ".jpg"
+file_name = sys.argv[1]
+time.sleep(1)
+camera.capture(file_header+file_name+file_footer)
+camera.stop_preview()
