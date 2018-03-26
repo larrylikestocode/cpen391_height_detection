9b7b07bd05fc446232e353955c32df0bfb7857b4
Larry Liu <yunpengliu199703@gmail.com>
2018-03-26 07:27:07 +0000

    camera_pi.py

--- camera_pi.py
+++ camera_pi.py
@@ -7,6 +7,6 @@ camera.start_preview()
 file_header = "photos/"
 file_footer = ".jpg"
 file_name = sys.argv[1]
-time.sleep(1)
+time.sleep(10)
 camera.capture(file_header+file_name+file_footer)
 camera.stop_preview()
