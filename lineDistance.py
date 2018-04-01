3a370fec3d253c978224c4e363ac2dc2606f4109
unknown <yunpengliu199703@gmail.com>
2018-04-01 10:50:48 -0700

    testing

--- lineDistance.py
+++ lineDistance.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
-
+import time
+# TBD
 BEGINX = 0
-BEGINY = 0
+BEGINY = 250
 ENDX = 0
-ENDY = 0
+ENDY = 320
 # ----------------- 
 def readImageToRGB():
 	imgBGR = cv2.imread('photos/face3.jpg')
@@ -18,13 +19,19 @@ def avgRGBLine(imgRGB):
 	height = imgRGB.shape[0]
 	width  = imgRGB.shape[1]
 
-	# for x in range () 
-
+	for x in range (0, height):
+		sumRGB = np.array((0,0,0))
+		for y in range (BEGINY,ENDY):
+			sumRGB = sumRGB + img[x][y]
+		print("x value is the ", x)
+		print(sumRGB//(ENDY-BEGINY))
+		print("")
+		time.sleep(0.5)
 
 # -----------------
 # test draw 
 def testDraw(imgRGB):
-	
+
 
 
 
